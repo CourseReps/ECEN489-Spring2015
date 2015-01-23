6ff2d4c1759d5f90b31255ef0b5ee4c85b320c1a
Emil <emil@dhcp-10-202-119-66.tamulink.tamu.edu>
2015-01-23 16:48:06 -0600

    Added My First Java Program

---
+++ Students/emilthomas/HelloWorld/src/worldhello.java
@@ -0,0 +1,9 @@
+/**
+ * Created by emil on 1/23/15.
+ */
+public class worldhello {
+    public static int main(String args[]) {
+       System.out.print("Hello" + " World\n");
+        return 1;
+    }
+}
