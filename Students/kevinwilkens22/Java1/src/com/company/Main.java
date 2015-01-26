497969cc00d1b3fba201f073aa788d4e10f0aefc
kevinwilkens22 <kevinwilkens22@tamu.edu>
2015-01-26 17:52:14 -0600

    Created Project Java1 for assignment 1.

---
+++ Students/kevinwilkens22/Java1/src/com/company/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        double intAve;
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Enter the first number ");
+        a = userInput.nextInt();
+        System.out.print("Enter the second number ");
+        b = userInput.nextInt();
+
+        intAve = (a + b) / 2;
+        System.out.println("The average is " + intAve + "!");
+    }
+}
