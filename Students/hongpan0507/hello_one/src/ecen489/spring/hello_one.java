884e48dd6bb769ed6cc59d3af6063e2f9ae7dab8
hongpan0507 <hongpan0507@gmail.com>
2015-01-26 16:08:00 -0800

    hello one

--- Students/hongpan0507/hello_one/src/ecen489/spring/hello_one.java
+++ Students/hongpan0507/hello_one/src/ecen489/spring/hello_one.java
@@ -6,7 +6,6 @@ package ecen489.spring;
 public class hello_one {
     public static void main(String[] args) {
         System.out.println("hello one");
-        System.out.printf("test______");
 
     }
 }
