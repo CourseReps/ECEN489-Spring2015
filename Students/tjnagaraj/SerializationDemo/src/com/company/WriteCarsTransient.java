bb10a84474651adffbe12160772153526cd410a6
Nagaraj Thenkarai Janakiraman <tjnagaraj.ece@gmail.com>
2015-02-04 11:02:10 -0600

    Transient Demo files

---
+++ Students/tjnagaraj/SerializationDemo/src/com/company/WriteCarsTransient.java
@@ -0,0 +1,37 @@
+package com.company;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+/**
+ * Created by Nagaraj on 2/3/2015.
+ */
+public class WriteCarsTransient {
+
+    public static void main(String[] args) {
+        CarsTransient car1 = new CarsTransient();
+        car1.setMake("Nissan");
+        car1.setModel("Sentra");
+        car1.setYear(2014);
+
+        CarsTransient car2 = new CarsTransient();
+        car2.setMake("Chevrolet");
+        car2.setModel("Cruze");
+        car2.setYear(2013);
+// copying objects to Binary file
+        try {
+            FileOutputStream fos = new FileOutputStream("Carlist.bin");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(car1);
+            System.out.println("\n car1 copied to file.");
+            oos.writeObject(car2);
+            System.out.println("\n car2 copied to file. \n");
+            oos.close();
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
