16cc124bfafee35119ff215cc902eae2e800c048
Nagaraj Thenkarai Janakiraman <tjnagaraj.ece@gmail.com>
2015-02-03 18:06:51 -0600

    Serialization Demo files upload

---
+++ Students/tjnagaraj/SerializationDemo/src/com/company/Cars.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.io.Serializable;
+
+/**
+ * Created by NAGARAJ on 2/3/2015.
+ * For Serialization concepts demo
+ */
+public class Cars implements Serializable{
+        private String make;
+        private int year;
+        private String model;
+
+        public void setModel(String model) {
+            this.model = model;
+        }
+
+        public void setYear(int year) {
+            this.year = year;
+        }
+
+        public void setMake(String make) {
+            this.make = make;
+        }
+
+        public String getModel(){
+            return model;
+        }
+        public int getYear(){
+            return year;
+        }
+        public String getMake(){
+            return make;
+        }
+
+        public void printModel(){
+            System.out.println("Name: "+model);
+        }
+        public void printYear(){
+            System.out.println("Year: "+year);
+        }
+        public  void printMake(){
+            System.out.println("Make: "+make);
+        }
+
+}
