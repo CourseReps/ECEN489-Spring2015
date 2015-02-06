70f22b5c8ead33e86330734365a1cb3fb79b38b7
BenitoRamirezJr <benitoramirez@tamu.edu>
2015-02-05 22:35:58 -0600

    Loading up Assignment 3

--- Students/BenitoRamirezJr/Assignment3Server/src/Server.java
+++ Students/BenitoRamirezJr/Assignment3Server/src/Server.java
@@ -1,63 +1,43 @@
-/**
- * Created by Benito on 2/4/2015.
- */
-/*import java.util.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Properties;
-
-public class Server {
-
-        public static void main(String[] args) {
-            // Code to print Date and Time
-            System.out.println("Date and Time:");
-            Date date = new Date();
-            System.out.println(date.toString());
-            // Printing System Properties
-            System.out.println("System Properties:");
-            System.out.println("Java Class path: "+ System.getProperty("java.class.path"));
-            System.out.println("JRE vendor name: "+ System.getProperty("java.vendor"));
-            System.out.println("OS architecture: "+ System.getProperty("os.arch"));
-            System.out.println("OS name: "+ System.getProperty("os.name"));
-            System.out.println("OS version: "+ System.getProperty("os.version"));
-            System.out.println("User home directory: "+ System.getProperty("user.home"));
-            System.out.println("User account name: "+ System.getProperty("uer.name"));
-        }
-    }*/
+import java.io.*;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
 
-/**
- * A TCP server that runs on port 9090.  When a client connects, it
- * sends the client the current date and time, then closes the
- * connection with that client.  Arguably just about the simplest
- * server you can write.
- */
-public class Server {
+public class Server{
 
-    /**
-     * Runs the server.
-     */
     public static void main(String[] args) throws IOException {
-        ServerSocket listener = new ServerSocket(9090);
+        ServerSocket server = new ServerSocket(60025, 1);
+        int stop = 0;
         try {
-            while (true) {
-                Socket socket = listener.accept();
+            while (stop == 0) {
+                Socket socket = server.accept();
+
                 try {
-                    PrintWriter out =
-                            new PrintWriter(socket.getOutputStream(), true);
-                    out.println(new Date().toString());
-                } finally {
+
+                    BufferedReader input =
+                            new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                    String answer = input.readLine() + "\n" + input.readLine()
+                            + "\n" + input.readLine() + "\n" + input.readLine()
+                            + "\n" + input.readLine() + "\n" + input.readLine()
+                            + "\n" + input.readLine() + "\n" + input.readLine();
+
+                    System.out.printf(answer);
+
+                }
+                catch ( IOException ioException )
+                {
+                    System.out.println("\nError writing object");
+                }
+                finally {
                     socket.close();
+                    stop++;
                 }
             }
         }
+
         finally {
-            listener.close();
+            server.close();
         }
     }
 }
