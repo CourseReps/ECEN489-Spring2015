e0b0357139b3a2a1db659a6008e44f526c83fad5
Nagaraj Thenkarai Janakiraman <tjnagaraj.ece@gmail.com>
2015-02-11 11:14:43 -0600

    Assignment4 update

--- Students/tjnagaraj/Assignment4android/app/src/main/java/com/nagaraj/assignment4android/SystemProperties.java
+++ Students/tjnagaraj/Assignment4android/app/src/main/java/com/nagaraj/assignment4android/SystemProperties.java
@@ -1,17 +1,116 @@
 package com.nagaraj.assignment4android;
 
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class SystemProperties extends ActionBarActivity {
+public class SystemProperties extends ActionBarActivity implements SensorEventListener,LocationListener {
+
+    SensorManager sensorManager;
+    Sensor sensor;
+    LocationManager locationManager;
+    String provider;
+    TextView displayDate;
+    TextView displayOrientation;
+    TextView displayLocation;
+    TextView displayWifi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_system_properties);
+
+        LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+        boolean enabled = service
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+// check if enabled and if not send user to the GSP settings
+// Better solution would be to display a dialog and suggesting to
+// go to the settings
+        if (!enabled) {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        if (location != null) {
+            onLocationChanged(location);
+        } else {
+            displayLocation.setText("Location not available");
+        }
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        displayOrientation = (TextView) findViewById(R.id.display_orientation);
+        displayDate = (TextView) findViewById(R.id.display_date);
+        displayLocation = (TextView) findViewById(R.id.display_location);
+        displayWifi = (TextView) findViewById(R.id.display_wifi);
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+
+            sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        } else {
+            //No ACCELEROMETER found.
+
+            displayOrientation.setText("No ACCELEROMETER found!");
+        }
+
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+        locationManager.removeUpdates(this);
+
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        int lat = (int) (location.getAltitude());
+        int lng = (int) (location.getLongitude());
+        displayLocation.setText("Latitude: " + lat + "   Longitude: " + lng);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled new provider " + provider, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Disabled provider " + provider,Toast.LENGTH_SHORT).show();
+
     }
 
 
@@ -30,10 +129,18 @@ public class SystemProperties extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        if (id == R.id.action_settings) return true;
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
