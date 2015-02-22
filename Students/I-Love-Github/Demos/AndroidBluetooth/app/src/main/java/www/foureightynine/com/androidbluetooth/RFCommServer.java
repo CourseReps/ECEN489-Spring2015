42b5afcd12f98e73820ae6a87319a30aedb2e746
I-Love-Github <kindred192@tamu.edu>
2015-02-21 18:41:58 -0600

    Updated with a working bluetooth code
    The code can be found under Students/I-Love-Github/Demos/HelloBluetooth
    and /AndroidBluetooth
    
    This code pushes a single string from the client (PC) to a server
    (android device), which is then displayed on the app screen.  The
    connection emulates a traditional Java socket, and can be expanded upon
    to create a complete data connection

---
+++ Students/I-Love-Github/Demos/AndroidBluetooth/app/src/main/java/www/foureightynine/com/androidbluetooth/RFCommServer.java
@@ -0,0 +1,103 @@
+package www.foureightynine.com.androidbluetooth;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.util.Log;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.UUID;
+
+public class RFCommServer extends Thread {
+
+    //based on java.util.UUID
+    private static UUID MY_UUID = UUID.fromString("446118f0-8b1e-11e2-9e96-0800200c9a66");
+
+    // The local server socket
+    private BluetoothServerSocket mmServerSocket;
+
+    // based on android.bluetooth.BluetoothAdapter
+    private BluetoothAdapter mAdapter;
+    private BluetoothDevice remoteDevice;
+
+    private Activity activity;
+
+    public RFCommServer(Activity activity) {
+        this.activity = activity;
+    }
+
+    public void run() {
+        BluetoothSocket socket = null;
+
+        // Listen to the server socket if we're not connected
+        while (true) {
+
+            mAdapter = BluetoothAdapter.getDefaultAdapter();
+
+            try {
+                // Create a new listening server socket
+                Log.d(this.getName(), "Initializing RFCOMM SERVER....");
+
+                // MY_UUID is the UUID you want to use for communication
+
+                mmServerSocket = mAdapter.listenUsingRfcommWithServiceRecord("Simple SPP", MY_UUID);
+                //mmServerSocket = mAdapter.listenUsingInsecureRfcommWithServiceRecord(NAME, MY_UUID); // you can also try using In Secure connection...
+
+                // This is a blocking call and will only return on a
+                // successful connection or an exception
+                socket = mmServerSocket.accept();
+
+                Thread.sleep(100);
+            } catch (Exception e) {
+
+                Log.e(this.getName(), "Error creating socket: " + e.getMessage());
+            }
+
+
+            try {
+                Log.d(this.getName(), "Closing Server Socket.....");
+                mmServerSocket.close();
+
+                InputStream tmpIn = null;
+                OutputStream tmpOut = null;
+
+                // Get the BluetoothSocket input and output streams
+
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+
+
+                BufferedReader readInStream = new BufferedReader(new InputStreamReader(tmpIn));
+//                DataInputStream mmInStream = new DataInputStream(tmpIn);
+                DataOutputStream mmOutStream = new DataOutputStream(tmpOut);
+
+                final String newText = readInStream.readLine();
+                // here you can use the Input Stream to take the string from the client whoever is connecting
+                //similarly use the output stream to send the data to the client
+
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        RelativeLayout layout = (RelativeLayout) activity.findViewById(R.id.relativeLayout);
+                        TextView text = (TextView) layout.findViewById(R.id.textView);
+
+                        text.setText(newText);
+                    }
+                });
+
+            } catch (Exception e) {
+
+                Log.e(this.getName(), "Error creating socket: " + e.getMessage());
+            }
+        }
+    }
+}
