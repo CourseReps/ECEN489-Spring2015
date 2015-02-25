f8c37ef09d53864972a6656f05d38ea97ea21193
Nagaraj Thenkarai Janakiraman <tjnagaraj.ece@gmail.com>
2015-02-25 17:02:33 -0600

    Least Squares based Regression Implementation

---
+++ Students/tjnagaraj/Project1/LinearRegressionLS/src/com/company/LinearRegressionLS.java
@@ -0,0 +1,76 @@
+package com.company;
+
+import org.ejml.alg.dense.linsol.svd.SolvePseudoInverseSvd;
+import org.ejml.alg.dense.misc.TransposeAlgs;
+import org.ejml.alg.dense.mult.MatrixMatrixMult;
+import org.ejml.data.DenseMatrix64F;
+
+public class LinearRegressionLS {
+    private long[] numberOfMac;
+    private long[] numberOfPeople;
+    // coefficients where the final result will be collected
+    private double[] coefficients={0,0};
+
+    LinearRegressionLS(long[] numberOfMac, long[] numberOfPeople){
+        this.numberOfMac=numberOfMac;
+        this.numberOfPeople=numberOfPeople;
+    //    A= new DenseMatrix64F(numberOfMac.length,2);
+    }
+    public double[] getCoefficients(){
+
+        DenseMatrix64F matrixA =new DenseMatrix64F(numberOfMac.length,2);
+        //contains the number of people values
+        DenseMatrix64F matrixB= new DenseMatrix64F(numberOfPeople.length,1);
+        //Contains transpose(A)*A
+        DenseMatrix64F matrixATranA=new DenseMatrix64F(2,2);
+        //Contains transpose(A)*B
+        DenseMatrix64F matrixATranB=new DenseMatrix64F(2,1);
+        //Contains Inverse(transpose(A)*A)
+        DenseMatrix64F matrixATranAInv = new DenseMatrix64F(2,2);
+        //Contains the final result
+        DenseMatrix64F matrixCoefficients = new DenseMatrix64F(2,1);
+        //Defining matrixA
+        for(int i=0;i<numberOfMac.length;i++){
+            matrixA.set(i,0,1);
+            matrixA.set(i,1,numberOfMac[i]);
+            matrixB.set(i,0,numberOfPeople[i]);
+        }
+
+        MatrixMatrixMult.mult_small(transpose(matrixA),matrixA,matrixATranA);
+        MatrixMatrixMult.mult_small(transpose(matrixA),matrixB,matrixATranB);
+        SolvePseudoInverseSvd pinv =new SolvePseudoInverseSvd();
+        pinv.setA(matrixATranA);
+        pinv.invert(matrixATranAInv);
+        MatrixMatrixMult.mult_small(matrixATranAInv, matrixATranB, matrixCoefficients);
+        //Converting the result to double[] format
+        coefficients[0]=matrixCoefficients.get(0,0);
+        coefficients[1]=matrixCoefficients.get(1,0);
+        return coefficients;
+    }
+    //Simple function that computes the transpose
+   private DenseMatrix64F transpose(DenseMatrix64F matrixA){
+        DenseMatrix64F matrixATranspose =new DenseMatrix64F(matrixA.getNumCols(),matrixA.getNumRows());
+        TransposeAlgs.standard(matrixA, matrixATranspose);
+     //  System.out.println(matrixATranspose.toString());
+        return  matrixATranspose;
+    }
+
+
+    public static void main(String[] args) {
+	// write your code here
+        double[] coefficentsLS;
+        long numberOfMac[]={1,2,3,5,6,7};
+        long numberOfPeople[]={3,5,7,11,13,17};
+        LinearRegressionLS ls =new LinearRegressionLS(numberOfMac,numberOfPeople);
+
+        coefficentsLS=ls.getCoefficients();
+
+        // Setting precision to five decimal places
+        for(int i=0;i<2;i++) {
+            String s = String.format("%.5f", coefficentsLS[i]);
+            coefficentsLS[i] = Double.parseDouble(s);
+        }
+        System.out.println("The best fit line based on Least Squares approximation is:  (# of people in the area) = "+coefficentsLS[0]+" + " + coefficentsLS[1]+" * (# of MAC Addresses) ");
+    }
+
+}
