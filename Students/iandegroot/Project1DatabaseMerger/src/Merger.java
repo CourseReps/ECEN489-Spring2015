8062898c288bf54723b2617e7742db20eb8d9da9
unknown <ian.degroot@yahoo.com>
2015-03-02 15:45:38 -0600

    Updated merger and server

--- Students/iandegroot/Project1DatabaseMerger/src/Merger.java
+++ Students/iandegroot/Project1DatabaseMerger/src/Merger.java
@@ -3,39 +3,49 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 
 public class Merger {
 
     public static void main(String[] args) {
         String sql;
         boolean creatingDB;
+        String newDB = "test2.db";
+        String finalDB = "final.db";
+
+        SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
         //Declare database variables
-        Connection c = null, c2 = null;
-        Statement stmt = null;
+        Connection dbIN, dbOUT, dbFinal;
+        Statement stmt;
 
         String s = System.getProperty("user.dir");
 
         //Check if the db file must be created
-        File dbfile = new File(s + "\\test.db");
+        File dbfile = new File(s + "\\" + newDB);
         creatingDB = !dbfile.exists();
 
         try {
             //Connect to the database
             Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:test.db");
-            c.setAutoCommit(false);
+            dbIN = DriverManager.getConnection("jdbc:sqlite:testing.db");
+            dbIN.setAutoCommit(false);
 
             //Class.forName("org.sqlite.JDBC");
-            c2 = DriverManager.getConnection("jdbc:sqlite:clicks.db");
-            c2.setAutoCommit(false);
+            dbOUT = DriverManager.getConnection("jdbc:sqlite:" + newDB);
+            dbOUT.setAutoCommit(false);
 
-            System.out.println("Opened database successfully");
+            //Class.forName("org.sqlite.JDBC");
+            dbFinal = DriverManager.getConnection("jdbc:sqlite:" + finalDB);
+            dbFinal.setAutoCommit(false);
+
+            System.out.println("Opened databases successfully");
 
             //If the db file was created then create the table CLICKS
             if (creatingDB) {
-                stmt = c.createStatement();
-                sql = "CREATE TABLE TESTING (X DOUBLE, Y DOUBLE)";
+                stmt = dbOUT.createStatement();
+                //sql = "CREATE TABLE DATA (ID INTEGER PRIMARY KEY, TIMES LONG, MAC TEXT, PBID LONG)";
+                sql = "CREATE TABLE DATA (PBID INTEGER, TIME LONG, MAC TEXT)";
                 stmt.executeUpdate(sql);
             }
 
@@ -48,30 +58,95 @@ public class Merger {
 //                //System.out.println("Point (" + p.getX() + ", " + p.getY() + ") received from client and loaded into database");
 //            }
 
-            stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery( "SELECT * FROM TESTING;" );
+            //Get the PBID which indicates which PB the data came from
+            stmt = dbIN.createStatement();
+            ResultSet rootRS = stmt.executeQuery( "SELECT * FROM PBID;" );
 
-            while (rs.next()) {
-                double x = rs.getDouble("X");
-                double y = rs.getDouble("Y");
+            long pbid = rootRS.getLong("ID");
 
-                System.out.println("X: " + x + " Y: " + y);
+            //Delete any duplicate data
+            stmt = dbIN.createStatement();
+            sql = "DELETE FROM DATA WHERE ROWID NOT IN (SELECT MIN(ROWID) FROM DATA GROUP BY TIME, MAC);";
+            stmt.executeUpdate(sql);
 
-                stmt = c2.createStatement();
-                sql = "INSERT INTO TESTING (X, Y) VALUES (" + x + ", " + y + ");";
+            //Get the first time
+            stmt = dbIN.createStatement();
+            ResultSet dataRS = stmt.executeQuery( "SELECT * FROM DATA;" );
+
+
+            //long pbid = 1000;
+            long time;
+            String mac;
+
+            /*
+            Get min time
+            while
+             */
+
+            while (dataRS.next()) {
+                time = dataRS.getLong("TIME");
+                mac = dataRS.getString("MAC");
+
+                stmt = dbOUT.createStatement();
+                sql = "INSERT INTO DATA (PBID, TIME, MAC) VALUES (" + pbid + ", " + time + ", '" + mac + "');";
                 stmt.executeUpdate(sql);
             }
 
-//            stmt = c.createStatement();
-//            sql = "INSERT INTO TESTING SELECT * FROM toMerge.TESTING";
-//            stmt.executeUpdate(sql);
+            //Check if the db file must be created
+            File dbFinalfile = new File(s + "\\" + finalDB);
+            creatingDB = !dbFinalfile.exists();
+
+            //If the db file was created then create the table CLICKS
+            //if (creatingDB) {
+                stmt = dbFinal.createStatement();
+                //sql = "CREATE TABLE DATA (ID INTEGER PRIMARY KEY, TIMES LONG, MAC TEXT, PBID LONG)";
+                sql = "CREATE TABLE DATA (TIME TEXT, NUM_MACS INTEGER, NUM_PEOPLE INTEGER, PBID INTEGER, ADDED TEXT)";
+                stmt.executeUpdate(sql);
+           // }
+
+
+            stmt = dbOUT.createStatement();
+            ResultSet timeRS = stmt.executeQuery( "SELECT * FROM DATA WHERE ROWID = 1;");
+            long startTime = timeRS.getLong("TIME");
+
+            //stmt = dbOUT.createStatement();
+            timeRS = stmt.executeQuery( "SELECT * FROM DATA ORDER BY TIME DESC LIMIT 1;");
+            long endTime = timeRS.getLong("TIME");
+
+            ResultSet finalRS;
+            long end;
+            int macCtr;
+
+            for(long i = startTime; i <= endTime; i += 10)
+            {
+                end = i + 10;
+                stmt = dbOUT.createStatement();
+                finalRS = stmt.executeQuery("SELECT * FROM DATA WHERE (TIME >= " + i + " AND TIME < " + end + ") GROUP BY MAC;");
+                //stmt.executeUpdate(sql);
+
+                if(!finalRS.isBeforeFirst())
+                    macCtr = 1;
+                else
+                    macCtr = 0;
+
+                while(finalRS.next())
+                    macCtr++;
+
+                stmt = dbFinal.createStatement();
+                sql = "INSERT INTO DATA (TIME, NUM_MACS, NUM_PEOPLE, PBID, ADDED) VALUES ('" + DATE_FORMAT.format(i * 1000) + "', " + macCtr + ", 0, " + pbid + ", 'NO');";
+                stmt.executeUpdate(sql);
+            }
+
+            //dbfile.delete();
 
             stmt.close();
-            c.commit();
-            c.close();
-            c2.commit();
-            c2.close();
-            System.out.println("Database closed");
+            dbIN.commit();
+            dbIN.close();
+            dbOUT.commit();
+            dbOUT.close();
+            dbFinal.commit();
+            dbFinal.close();
+            System.out.println("Databases closed");
         }
         catch ( Exception e ) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
--- Students/iandegroot/Project1LinuxServer/src/FileServer.java
+++ Students/iandegroot/Project1LinuxServer/src/FileServer.java
@@ -1,6 +1,12 @@
 import java.io.*;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
 
 public class FileServer {
 
@@ -11,18 +17,29 @@ public class FileServer {
     public static void main (String [] args ) throws IOException {
         int current, bytesRead;
 
+        String sql;
+        boolean creatingDB;
+
+        SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        InetAddress IP = InetAddress.getLocalHost();
+        System.out.println("IP of my system is: " + IP.getHostAddress());
+
+        System.out.println("Using port number: " + SOCKET_PORT);
+
         FileOutputStream fos = null;
         BufferedOutputStream bos = null;
         ServerSocket servsock = null;
         Socket sock = null;
+        PrintWriter out;
 
         try {
             servsock = new ServerSocket(SOCKET_PORT);
             while (true) {
-                System.out.println("Waiting...");
+                System.out.println("Waiting for an android device to connect...");
                 try {
                     sock = servsock.accept();
-                    System.out.println("Accepted connection : " + sock);
+                    System.out.println("Accepted connection: " + sock);
 
                     //Timestamp the file
                     //FILE_TO_RECEIVE += "\\" + System.currentTimeMillis() + sock.getInetAddress().getCanonicalHostName();
@@ -43,19 +60,121 @@ public class FileServer {
 
                     bos.write(mybytearray, 0, current);
                     bos.flush();
-                    System.out.println("File " + FILE_TO_RECEIVE + " downloaded (" + current + " bytes read)");
 
-                    FILE_TO_RECEIVE = System.getProperty("user.dir");
+                    File dbIN = new File(FILE_TO_RECEIVE);
+                    out = new PrintWriter(sock.getOutputStream(), true);
+
+                    if(dbIN.exists())
+                    {
+                        System.out.println("File " + FILE_TO_RECEIVE + " downloaded (" + current + " bytes read)");
+                        //out.print("Got file");
+                    }
+                    else
+                    {
+                        System.out.println("File " + FILE_TO_RECEIVE + " was not downloaded correctly");
+                        //out.print("There was an error downloading the file");
+                    }
+
+
                 }
                 finally {
                     if (fos != null) fos.close();
                     if (bos != null) bos.close();
                     if (sock!=null) sock.close();
                 }
+
+
+
+
+                //Declare database variables
+                Connection dbIN = null, dbOUT = null;
+                Statement stmt = null;
+
+                String s = System.getProperty("user.dir");
+
+                //Check if the db file must be created
+                File dbfile = new File(s + "\\main.db");
+                creatingDB = !dbfile.exists();
+
+                try {
+                    //Connect to the database
+                    Class.forName("org.sqlite.JDBC");
+                    dbIN = DriverManager.getConnection("jdbc:sqlite:" + FILE_TO_RECEIVE);
+                    dbIN.setAutoCommit(false);
+
+                    //Class.forName("org.sqlite.JDBC");
+                    dbOUT = DriverManager.getConnection("jdbc:sqlite:main.db");
+                    dbOUT.setAutoCommit(false);
+
+                    System.out.println("Opened databases successfully");
+
+                    //If the db file was created then create the table CLICKS
+                    if (creatingDB) {
+                        stmt = dbOUT.createStatement();
+                        sql = "CREATE TABLE DATA (ID INTEGER PRIMARY KEY, TIMES LONG, MAC TEXT, PBID LONG)";
+                        stmt.executeUpdate(sql);
+                    }
+
+                    //Get Points from the client and load them into the database
+//            for (int i = 0; i < 10; i++) {
+//                stmt = c.createStatement();
+//                sql = "INSERT INTO TESTING (X, Y) VALUES (" + i + ", " + i + ");";
+//                stmt.executeUpdate(sql);
+//
+//                //System.out.println("Point (" + p.getX() + ", " + p.getY() + ") received from client and loaded into database");
+//            }
+
+                    stmt = dbIN.createStatement();
+                    ResultSet rootRS = stmt.executeQuery( "SELECT * FROM ROOT;" );
+
+                    long pbid = rootRS.getLong("PBID");
+
+                    stmt = dbIN.createStatement();
+                    ResultSet dataRS = stmt.executeQuery( "SELECT * FROM DATA;" );
+                    //long pbid = 1000;
+                    long times;
+                    String mac;
+                    int id = 0;
+
+                    while (dataRS.next()) {
+                        id++;
+                        times = dataRS.getLong("TIMES");
+                        mac = dataRS.getString("MAC");
+
+                        //System.out.println("X: " + x + " Y: " + y);
+
+                        stmt = dbOUT.createStatement();
+                        sql = "INSERT INTO DATA (TIMES, MAC, PBID) VALUES (" + times + ", '" + mac + "', " + pbid + ");";
+                        stmt.executeUpdate(sql);
+
+
+                    }
+
+//            stmt = c.createStatement();
+//            sql = "INSERT INTO TESTING SELECT * FROM toMerge.TESTING";
+//            stmt.executeUpdate(sql);
+
+                    stmt.close();
+                    dbIN.commit();
+                    dbIN.close();
+                    dbOUT.commit();
+                    dbOUT.close();
+                    System.out.println("Databases closed");
+                }
+                catch ( Exception e ) {
+                    System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+                    System.exit(0);
+                }
+
+                FILE_TO_RECEIVE = System.getProperty("user.dir");
             }
         }
         finally {
             if (servsock != null) servsock.close();
         }
+
+
+
+
     }
 }
