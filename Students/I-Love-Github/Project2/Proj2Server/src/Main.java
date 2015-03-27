aee593e5fadb9bb760cba239ae3fbff2075c8a63
George Sullivan <kindred192@tamu.edu>
2015-03-27 02:59:12 -0500

    Oops
    added code to actually start the server thread

--- Students/I-Love-Github/Project2/Proj2Server/src/Main.java
+++ Students/I-Love-Github/Project2/Proj2Server/src/Main.java
@@ -2,6 +2,8 @@ class Main {
 
     public static void main(String args[]) {
 
-        ServerRunnable client = new ServerRunnable();
+        ServerRunnable client = new ServerRunnable(); 
+		Thread clientThread = new Thread(client);
+		clientThread.start();
     }
 }
