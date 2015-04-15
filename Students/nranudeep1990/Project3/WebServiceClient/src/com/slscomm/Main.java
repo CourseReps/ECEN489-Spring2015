83103e21b090fcf4946753a1d98c60d3360ed15f
nranudeep1990 <nranudeep_1990@email.tamu.edu>
2015-04-15 11:04:45 -0500

    Web Service Client stub
    Intellij Java APP

---
+++ Students/nranudeep1990/Project3/WebServiceClient/src/com/slscomm/Main.java
@@ -0,0 +1,15 @@
+package com.slscomm;
+
+import com.slscomm.ws.CommHandler;
+import com.slscomm.ws.CommHandlerImplService;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        CommHandlerImplService helloService = new CommHandlerImplService();
+        CommHandler hello = helloService.getCommHandlerImplPort();
+
+        System.out.println(hello.getHelloWorldAsString("15"));
+    }
+}
