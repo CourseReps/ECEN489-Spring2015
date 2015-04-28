1f53c075969f413706a3a541f6e4fb66e661e754
Nagaraj Thenkarai Janakiraman <tjnagaraj.ece@gmail.com>
2015-04-27 20:30:07 -0500

    Update DB

--- Students/iandegroot/Project3DBInterface/src/Main.java
+++ Students/iandegroot/Project3DBInterface/src/Main.java
@@ -1,8 +1,3 @@
-import java.lang.reflect.Array;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 
 /**
@@ -25,11 +20,11 @@ public class Main {
         dbHelper.addLocation("EIC", 12.567888, 12.567, "Image");
         dbHelper.addLocation("Rudder", 12.567888, 12.567, "Image");
         dbHelper.addLocation("Zachary", 12.567888, 12.567, "Image");
-        int mandelId = dbHelper.getUserIDByName("Mandel");
-        int trevorId = dbHelper.getUserIDByName("Trevor");
-        int benitoId = dbHelper.getUserIDByName("Benito");
-        int joshId = dbHelper.getUserIDByName("Josh");
-        int bladeId = dbHelper.getUserIDByName("Blade");
+        int mandelId = dbHelper.getUserIDByUserName("Mandel");
+        int trevorId = dbHelper.getUserIDByUserName("Trevor");
+        int benitoId = dbHelper.getUserIDByUserName("Benito");
+        int joshId = dbHelper.getUserIDByUserName("Josh");
+        int bladeId = dbHelper.getUserIDByUserName("Blade");
 
         dbHelper.addFriend(mandelId,trevorId);
         dbHelper.addFriend(mandelId,benitoId);
@@ -50,39 +45,39 @@ public class Main {
 
         System.out.println("Mandel's friends: " + mandelfriends.toString() + "\n");
 
-        dbHelper.checkIn(dbHelper.getUserIDByName("Mandel"), dbHelper.getLocationIDByName("Home"), 100000001, "Method");
-        dbHelper.checkIn(dbHelper.getUserIDByName("Mandel"), dbHelper.getLocationIDByName("School"), 100000002, "Method");
-        dbHelper.checkIn(dbHelper.getUserIDByName("Mandel"), dbHelper.getLocationIDByName("EIC"), 100000003, "Method");
-        dbHelper.checkIn(dbHelper.getUserIDByName("Mandel"), dbHelper.getLocationIDByName("Rudder"), 100000004, "Method");
-        dbHelper.checkIn(dbHelper.getUserIDByName("Mandel"), dbHelper.getLocationIDByName("Zachary"), 100000005, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Mandel"), dbHelper.getLocationIDByName("Home"), 100000001, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Mandel"), dbHelper.getLocationIDByName("School"), 100000002, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Mandel"), dbHelper.getLocationIDByName("EIC"), 100000003, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Mandel"), dbHelper.getLocationIDByName("Rudder"), 100000004, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Mandel"), dbHelper.getLocationIDByName("Zachary"), 100000005, "Method");
 
-        dbHelper.checkIn(dbHelper.getUserIDByName("Benito"), dbHelper.getLocationIDByName("Home"), 110000001, "Method");
-        dbHelper.checkIn(dbHelper.getUserIDByName("Benito"), dbHelper.getLocationIDByName("School"), 110000002, "Method");
-        dbHelper.checkIn(dbHelper.getUserIDByName("Benito"), dbHelper.getLocationIDByName("EIC"), 110000003, "Method");
-        dbHelper.checkIn(dbHelper.getUserIDByName("Benito"), dbHelper.getLocationIDByName("Rudder"), 110000004, "Method");
-        dbHelper.checkIn(dbHelper.getUserIDByName("Benito"), dbHelper.getLocationIDByName("Zachary"), 110000005, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Benito"), dbHelper.getLocationIDByName("Home"), 110000001, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Benito"), dbHelper.getLocationIDByName("School"), 110000002, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Benito"), dbHelper.getLocationIDByName("EIC"), 110000003, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Benito"), dbHelper.getLocationIDByName("Rudder"), 110000004, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Benito"), dbHelper.getLocationIDByName("Zachary"), 110000005, "Method");
 
-        dbHelper.checkIn(dbHelper.getUserIDByName("Trevor"), dbHelper.getLocationIDByName("Home"), 111000001, "Method");
-        dbHelper.checkIn(dbHelper.getUserIDByName("Trevor"), dbHelper.getLocationIDByName("School"), 111000002, "Method");
-        dbHelper.checkIn(dbHelper.getUserIDByName("Trevor"), dbHelper.getLocationIDByName("EIC"), 111000003, "Method");
-        dbHelper.checkIn(dbHelper.getUserIDByName("Trevor"), dbHelper.getLocationIDByName("Rudder"), 111000004, "Method");
-        dbHelper.checkIn(dbHelper.getUserIDByName("Trevor"), dbHelper.getLocationIDByName("Zachary"), 111000005, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Trevor"), dbHelper.getLocationIDByName("Home"), 111000001, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Trevor"), dbHelper.getLocationIDByName("School"), 111000002, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Trevor"), dbHelper.getLocationIDByName("EIC"), 111000003, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Trevor"), dbHelper.getLocationIDByName("Rudder"), 111000004, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Trevor"), dbHelper.getLocationIDByName("Zachary"), 111000005, "Method");
 
-        dbHelper.checkIn(dbHelper.getUserIDByName("Josh"), dbHelper.getLocationIDByName("Home"), 111100001, "Method");
-        dbHelper.checkIn(dbHelper.getUserIDByName("Josh"), dbHelper.getLocationIDByName("School"), 111100002, "Method");
-        dbHelper.checkIn(dbHelper.getUserIDByName("Josh"), dbHelper.getLocationIDByName("EIC"), 111100003, "Method");
-        dbHelper.checkIn(dbHelper.getUserIDByName("Josh"), dbHelper.getLocationIDByName("Rudder"), 111100004, "Method");
-        dbHelper.checkIn(dbHelper.getUserIDByName("Josh"), dbHelper.getLocationIDByName("Zachary"), 111100005, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Josh"), dbHelper.getLocationIDByName("Home"), 111100001, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Josh"), dbHelper.getLocationIDByName("School"), 111100002, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Josh"), dbHelper.getLocationIDByName("EIC"), 111100003, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Josh"), dbHelper.getLocationIDByName("Rudder"), 111100004, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Josh"), dbHelper.getLocationIDByName("Zachary"), 111100005, "Method");
 
-        dbHelper.checkIn(dbHelper.getUserIDByName("Blade"), dbHelper.getLocationIDByName("Home"), 111110001, "Method");
-        dbHelper.checkIn(dbHelper.getUserIDByName("Blade"), dbHelper.getLocationIDByName("School"), 111110002, "Method");
-        dbHelper.checkIn(dbHelper.getUserIDByName("Blade"), dbHelper.getLocationIDByName("EIC"), 111110003, "Method");
-        dbHelper.checkIn(dbHelper.getUserIDByName("Blade"), dbHelper.getLocationIDByName("Rudder"), 111110004, "Method");
-        dbHelper.checkIn(dbHelper.getUserIDByName("Blade"), dbHelper.getLocationIDByName("Zachary"), 111110005, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Blade"), dbHelper.getLocationIDByName("Home"), 111110001, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Blade"), dbHelper.getLocationIDByName("School"), 111110002, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Blade"), dbHelper.getLocationIDByName("EIC"), 111110003, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Blade"), dbHelper.getLocationIDByName("Rudder"), 111110004, "Method");
+        dbHelper.checkIn(dbHelper.getUserIDByUserName("Blade"), dbHelper.getLocationIDByName("Zachary"), 111110005, "Method");
 
 
         checkIns1 = dbHelper.getCheckInByLocation(dbHelper.getLocationIDByName("Home"));
-        checkIns2 = dbHelper.getCheckInByUser(dbHelper.getUserIDByName("Mandel"));
+        checkIns2 = dbHelper.getCheckInByUser(dbHelper.getUserIDByUserName("Mandel"));
         for(CheckIn ci : checkIns1){
             System.out.println("["+ci.getUserName()+","+ci.getLocationName()+"]");
         }
