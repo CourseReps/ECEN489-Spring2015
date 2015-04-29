e6f22b54c6a0ab26a7af844aeb3238fbe1539772
Blade Roybal <blade@elcloudo.tamu.edu>
2015-04-28 20:42:11 -0500

    updated project3 server code

---
+++ Students/kevinwilkens22/Project3/Project3DBInterface/src/CheckIn.java
@@ -0,0 +1,33 @@
+/**
+ * Created by mandel on 4/15/15.
+ */
+public class CheckIn {
+    private String userName;
+    private String locationName;
+    private int timestamp;
+    private String method;
+    
+    
+    public CheckIn(String userName, String locationName, int timestamp, String method){
+        this.userName = userName;
+        this.locationName = locationName;
+        this.timestamp = timestamp;
+        this.method = method;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public int getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+}
